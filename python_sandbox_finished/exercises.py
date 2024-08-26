595e01cdf9b9ea68746dd765ae5c447f7a358a5e
CMILINAZZO <cmilinazzo@meditech.com>
2024-08-26 16:41:26 -0400

    Update exercises.py

---
+++ python_sandbox_finished/exercises.py
@@ -0,0 +1,26 @@
+# Unit 1 Exercises & Homework Assignment
+
+# Exercise 1: Use the Python Interpreter to create different types of variables.
+# I created these variables using the Python Interpreter:
+
+x = 1
+y = 1.5
+name = 'Carrie'
+is_cool = True
+
+# Exercise 2: Use the Python Interpreter to try assigning values to variables and then using the conditionals and comparison operators.
+#I tried out these comparisons with my variables:
+
+x < y
+x > y
+x != y
+x == y
+x >= y
+x <= y
+
+# Exercise 3: Use VS Code to develop a Hello World program in Python.
+
+if name == 'Carrie':
+    print ("Hello World")
+
+# Homework: Create a python function that Calculates the area of a circle given its radius.
